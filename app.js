f8d0cc1f66096603631c26d9d6202f54365546d9
Stuart John Watson <ripp_@sucs.org>
2016-04-13 23:49:11 +0100

    started client

--- app.js
+++ app.js
@@ -6,19 +6,36 @@ var server = require('http').createServer(),
     app = express(),
     port = 3000;
 
+function client(ws,name){
+  this.ws = ws;
+  if (name === undefined){
+    this.after("name",{
+      type:"getName"
+    }).then(function(resp){
+      console.log(resp);
+    });
+  }
+}
+
+client.prototype.after = function(hook,data){
+  this.send(data);
+  return new Promise(function(resolve, reject) {
+    this.hooks[hook] = resolve;
+  });
+};
+
+client.prototype.send = function(data){
+  this.ws.send(JSON.stringify(data));
+};
+
 //Web socket
 wss.on('connection', function(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-  ws.send('something');
+  new client(ws);
 });
 
 //Express app
 app.use(express.static(__dirname + '/client'));
 
-
-
 //Bind to http server and start
 server.on('request', app);
-server.listen(port, function () { console.log('Listening on ' + server.address().port) });
+server.listen(port, function () { console.log('Listening on ' + server.address().port); });

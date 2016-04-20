d45d413bc111612f499652823a0ed584501267f2
Stuart John Watson <ripp_@sucs.org>
2016-04-20 23:06:42 +0100

    Added some documentation to Server/Game.js

--- server/Game.js
+++ server/Game.js
@@ -3,6 +3,11 @@ var fs = require("fs"),
     cards = require("./Card"),
     Grid = require("./Grid");
 
+/**
+ * Represents a game
+ * @param {Room} room     The parent Room
+ * @param {[String]} cardSets List of card sets to use
+ */
 function Game(room,cardSets){
   this.room = room;
   this.hands = [];
@@ -12,6 +17,10 @@ function Game(room,cardSets){
   this.grid.addCard([0,0],0); //DEMO
 }
 
+/**
+ * Loads a card set from the given file and adds them to the usable card List
+ * @param  {String}   file    File location to load the card list from
+ */
 Game.prototype.loadCards = function(file){
   var that = this;
   JSON.parse(fs.readFileSync(file)).forEach(function(rawCard){
@@ -21,6 +30,15 @@ Game.prototype.loadCards = function(file){
   });
 };
 
+/**
+ * Event handler to be called when a player plays a cards
+ * Calculates all the results of the play then returns what needs to be done
+ * @param  {Array}    Cards   List of Card id's and positions that where played
+ * @param  {string}   Effect  The name of the effect that has been triggered by the play
+ * @param  {[object]} Params  List of paramaters for the triggered effect, varies basied on effect
+ * @param  {Client}   Client  The client who made the play
+ * @return {Array}            List of Card id's and positions that should be reported back to the client to update their grid
+ */
 Game.prototype.onPlay = function(cards,effect,params,client){
   if(effect == "replace"){
     this.grid.replaceCard(params[0],cards[0].id);
